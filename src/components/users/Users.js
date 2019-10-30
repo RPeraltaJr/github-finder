c494ad31bf5370fb90528d77a914e2a918bb2ae4
Rafael Peralta Jr <rperaltajr@gmail.com>
2019-10-30 00:40:38 -0400

    Add Loading component as well as env vars for Github api credentials

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,43 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem';
+import Spinner from './../layout/Spinner';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-    state = {
-        users: [
-            {
-                id: 1,
-                login: "mojombo",
-                avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-                html_url: "https://github.com/mojombo",
-            },
-            {
-                id: 2,
-                login: "defunkt",
-                avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-                html_url: "https://github.com/defunkt",
-            },
-            {
-                id: 3,
-                login: "pjhyett",
-                avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-                html_url: "https://github.com/pjhyett",
-            },
-        ]
-    }
+const Users = ({ users, loading }) => {
 
-    render() {
+    if(loading) { 
+        return <Spinner />
+    } else {
         return (
             <div className="container mt-3">
                 <div className="row">
+                
                     {/* Rendering the child component multiple times */}
-                    { this.props.users.map(user => (
+                    { users.map(user => (
                         <UserItem key={user.id} user={user} />
                     )) }
-
+    
                 </div>
             </div>
         )
     }
+
+}
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
 }
 
 export default Users
