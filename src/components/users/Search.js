71ca0d6f6b0888e4b5f92b81d511f6c453cb8b61
Rafael Peralta Jr <rperaltajr@gmail.com>
2019-11-01 11:19:48 -0400

    Change Search to functional component

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,58 +1,51 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-class Search extends Component {
-    state = {
-        search: ''
-    }
+const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+    const [ search, setSearch ] = useState('');
 
-    static propTypes = {
-        searchUsers: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired,
-        setAlert: PropTypes.func.isRequired
-    }
+    const onChange = e => setSearch( e.target.value ); 
 
-    // name of input field is the same as what we're updating within State, hence we use [e.target.name]
-    onChange = e => this.setState({ [e.target.name]: e.target.value }); 
-
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
-        if( this.state.search === '' ) {
-            this.props.setAlert('Please enter something.', 'danger');
+        if( search === '' ) {
+            setAlert('Please enter something.', 'danger');
         } else {
-            this.props.searchUsers(this.state.search); // passing up the value through props
-            this.setState({ search: '' }); // empty the form after submit
+            searchUsers(search); // passing up the value through props
+            setSearch(''); // empty the form after submit
         }
     }
 
-    render() {
-
-        const { showClear, clearUsers } = this.props;
-
-        return (
-            <div>
-                <div className="container mt-3">
-                    <div className="row">
-                        <div className="col-12 col-lg-6">
-                            <form className="form" autoComplete="false" onSubmit={this.onSubmit} >
-                                <div className="form-row">
-                                    <div className="col-6">
-                                        <label htmlFor="search" className="sr-only">Search</label>
-                                        <input type="search" name="search" className="form-control" placeholder="Search Users..." value={this.state.search} onChange={this.onChange} />
-                                    </div>
-                                    <div className="col-6">
-                                        <button type="submit" className="btn btn-info mb-2 mr-2">Submit</button>
-                                        { showClear && <button className="btn btn-default mb-2" onClick={clearUsers} >Clear</button> }
-                                    </div>
+    return (
+        <div>
+            <div className="container mt-3">
+                <div className="row">
+                    <div className="col-12 col-lg-6">
+                        <form className="form" autoComplete="false" onSubmit={onSubmit} >
+                            <div className="form-row">
+                                <div className="col-6">
+                                    <label htmlFor="search" className="sr-only">Search</label>
+                                    <input type="search" name="search" className="form-control" placeholder="Search Users..." value={search} onChange={onChange} />
                                 </div>
-                            </form>
-                        </div>
+                                <div className="col-6">
+                                    <button type="submit" className="btn btn-info mb-2 mr-2">Submit</button>
+                                    { showClear && <button className="btn btn-default mb-2" onClick={clearUsers} >Clear</button> }
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+
+}
+
+Search.propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
 }
 
 export default Search
