64ebab0fbaff58fc733e067862fbe9f629090288
Rafael Peralta Jr <rperaltajr@gmail.com>
2019-11-02 02:06:19 -0400

    Refactor. Create Home and 404 page

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound() {
+    return (
+        <div className="container mt-3">
+            <h1>Oops!</h1>
+            <p>The page you're looking for can't be found.</p>
+        </div>
+    )
+}
