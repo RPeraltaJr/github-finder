1544141c48554b21719e4f441e455cf6a5da8be7
Rafael Peralta Jr <rperaltajr@gmail.com>
2019-11-02 00:45:48 -0400

    Setup Context, Reducer & Actions

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -23,55 +23,21 @@ const GithubState = props => {
     const [state, dispatch] = useReducer(GithubReducer, initialState);
 
     // * Search Github Users
-    const searchUsers = async (search) => {
-        // * if not empty
-        if( search.trim() !== '' ) { 
-            setLoading();
-            const res = await axios.get(`https://api.github.com/search/users?q=${search}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-            dispatch({
-                type: SEARCH_USERS,
-                payload: res.data.items
-            });
-        } 
-    }
 
     // * Get Single Github User
-    const getUser = async (username) => {
-        setLoading();
-        const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-        dispatch({
-            type: GET_USER,
-            payload: res.data.items
-        });
-    }
 
     // * Get User Repos
-    const getUserRepos = async (username) => {
-        setLoading();
-        // setAlert(null);
-        const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=10&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-        dispatch({
-            type: GET_REPOS,
-            payload: res.data
-        });
-    }
 
     // * Clear Users
-    const clearUsers = () => dispatch({ type: CLEAR_USERS });
 
     // * Set Loading
-    const setLoading = () => dispatch({ type: SET_LOADING });
 
     // * Return the provider (with anything we want available throughout the entire app)
     return <GithubContext.Provider value={{
-            users: state.users,
-            user: state.user,
-            repos: state.repos,
-            loading: state.loading,
-            searchUsers,
-            clearUsers,
-            getUser,
-            getUserRepos,
+        users: state.users,
+        user: state.user,
+        repos: state.repos,
+        loading: state.loading,
     }}>
         { props.children }
     </GithubContext.Provider>
